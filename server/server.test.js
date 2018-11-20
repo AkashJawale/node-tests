e620afaa78c188ec05c32ea207e7f1598a905232
AkashJawale <akashjawale131@gmail.com>
2018-11-20 13:12:51 +0530

    Organizing test with describe()

--- server/server.test.js
+++ server/server.test.js
@@ -3,7 +3,10 @@ const expect = require('expect');
 
 var app = require('./server').app;
 
-it('should return hello world response', (done) => {
+describe('server', () => {
+
+describe('GET /', () => {
+	it('should return hello world response', (done) => {
   request(app)
     .get('/')
     .expect(404)
@@ -14,11 +17,10 @@ it('should return hello world response', (done) => {
     })
     .end(done);
 });
+});
 
-// Make a new test
-// assert 200
-// Assert that you exist in users array
-it('should return my user object', (done) => {
+	describe('GET /users', () => {
+		it('should return my user object', (done) => {
   request(app)
     .get('/users')
     .expect(200)
@@ -30,3 +32,7 @@ it('should return my user object', (done) => {
     })
     .end(done);
 });
+	});
+
+});
+
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -2,15 +2,18 @@ const expect = require('expect');
 
 const utils = require('./utils');
 
-it('should add two numbers', () => {
+describe('Utils', () => {
+describe('#add', () => {
+	it('should add two numbers', () => {
 var res = utils.add(33, 11);
 
 expect(res).toBe(44).toBeA('number');
 
-// if (res !== 44) {
-// 	throw new Error(`Expected 44, but got ${res}.`)
-// }
 });
+});
+
+
+
 
 
 it('should async add two numbers', (done) => {
@@ -35,6 +38,9 @@ it('should async square a number', (done) => {
     done();
   });
 });
+
+});
+
 
 
 //should verify first and last name are set
